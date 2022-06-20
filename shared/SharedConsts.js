f394c17cfa488097d3e61d23ba7900e6ee51e911
rodrigosinistro <80776011+rodrigosinistro@users.noreply.github.com>
2022-06-20 17:06:49 -0300

    Add files via upload

---
+++ shared/SharedConsts.js
@@ -0,0 +1,18 @@
+class SharedConsts {}
+
+SharedConsts.MODULE_TITLE = "Plutonium";
+SharedConsts.MODULE_TITLE_FAKE = "SRD: Enhanced";
+SharedConsts.MODULE_NAME = "plutonium";
+SharedConsts.MODULE_NAME_FAKE = "srd5e";
+SharedConsts.MODULE_DIR = "./dist/plutonium";
+
+SharedConsts.PACK_NAME_CREATURES = "creatures";
+SharedConsts.PACK_NAME_SPELLS = "spells";
+SharedConsts.PACK_NAME_ITEMS = "items";
+SharedConsts.PACK_CREATURES = `./packs/${SharedConsts.PACK_NAME_CREATURES}.db`;
+SharedConsts.PACK_SPELLS = `./packs/${SharedConsts.PACK_NAME_SPELLS}.db`;
+SharedConsts.PACK_ITEMS = `./packs/${SharedConsts.PACK_NAME_ITEMS}.db`;
+
+SharedConsts.MODULE_LOCATION = `modules/${SharedConsts.MODULE_NAME}`;
+
+export {SharedConsts};
